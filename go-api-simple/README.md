f5fec6a1f8c692fd3e487a6a3ca946d71d85b5a5
tahayk3 <tahayk3@gmail.com>
2024-07-22 14:13:00 -0600

    Agregando mux y una solicitud post para la api

--- go-api-simple/README.md
+++ go-api-simple/README.md
@@ -1,9 +1,26 @@
-Api simple RESTful con GO
+# Api simple RESTful con GO
 
-Esta cuenta con tres funciones:
 
-homePage: funcion para manejar todas las solicitudes, endpoint de la pagina principal
 
-handleRequest: Manejo de solicitudes
+# Synchronization
 
-Funcion principal: punto de entreda del programa(servidor), donde se llama a la funcion que escucha solicitudes
+Esta cuenta con cinco funciones:
+- homePage: funcion para manejar todas las solicitudes, endpoint de la pagina principal
+- handleRequest: Manejo de solicitudes
+- Funcion principal: punto de entreda del programa(servidor), donde se llama a la funcion que escucha solicitudes
+- Las dos funciones restantes, se utilizan para simular una solicitud get y post
+	> Se utiliza mux
+
+# De tener problemas con "github.com/gorilla/mux"
+Es necesario crear un entorno con go por lo general, este se crea el C:\Users\nombre-de-usuario\go\src\github.com\
+
+Se deben utilizar los siguientes comandos:
+
+mkdir -p $env:GOPATH\src\github.com\nombreusuario\nombre-del-proyecto
+
+go mod init github.com/tu-usuario/nombre-del-proyecto
+
+go get -u github.com/gorilla/mux
+
+De esta manera, se crean los archivos go.mod y go.sum
+
--- go-api-simple/main.go
+++ go-api-simple/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 /*
@@ -32,6 +34,11 @@ func allArticles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(articles)
 }
 
+// funcion que podria usarse para insertar datos.
+func PostArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Simulacion de insertar datos")
+}
+
 // endpoint de la pagina principal
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Acceso al punto final de la página principal")
@@ -39,9 +46,12 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 // Manejo de solicitudes
 func handleRequest() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	//Al utilizar mux, se puede especificar que tipo de solicitudes podra recibir un endpoint
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", PostArticles).Methods("POST")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 // Punto de entrada donde se llama a la funcion que escucha solicitudes
